a61b49e7fb23796f60788330a1e85bb6b15d9288
Guillermo VR <guillermo.vaca07@gmail.com>
2025-10-01 05:02:08 -0400

    solucionado login

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AuthProvider } from './context/AuthContext.jsx'; // Importa el AuthProvider
-import './services/api.js'; // Mantén tu import de la configuración de Axios
+import { AuthProvider } from './context/AuthContext.jsx';
+import './services/api.js'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* AuthProvider envuelve a App para darle a toda la app
-        acceso al contexto de autenticación */}
     <AuthProvider>
       <App />
     </AuthProvider>
