9553bea0950cd957500ebb5cc50194aaed1ec6f3
Guillermo VR <guillermo.vaca07@gmail.com>
2025-12-18 08:02:55 -0400

    subir vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000'; // O la URL de tu backend
-//axios.defaults.baseURL = 'http://3.129.13.240:8000';
+//axios.defaults.baseURL = 'http://localhost:8000'; // O la URL de tu backend
+axios.defaults.baseURL = 'https://condominio-backend-ebzb.onrender.com';
